6ca4c94e7f4df8682cf0fb5da0895706de6f2b5f
Tormod Flesjø <tormodflesjo@Tormods-MacBook-Pro.local>
2018-08-22 19:56:11 +0200

    is this even working

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,13 +6,37 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {beers});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', beers[0]);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.listen(3000, () => console.log('PORT 3000 AT YOUR SERVICE!'));
+
+
+
